51a719ce36c581ee8148fb4013afb22521db6ec0
Falsin <greenldend@gmail.com>
2021-02-11 16:51:34 +0700

    setting section block

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 const nav = document.querySelector('nav');
+const menuBlock = document.querySelector('.menuBlock');
 
-nav.onclick = () => nav.classList.toggle('active');
+nav.onclick = () => {
+  nav.classList.toggle('active');
+  menuBlock.classList.toggle('active');
+}
+
+const section = document.querySelector('.section');
+
+console.dir(section)
